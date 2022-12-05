0f72acdb5c38bdaf7f7b54644bd7a7fde13aef1a
Tomy Guichard <tomy2e@live.fr>
2022-12-05 11:31:34 +0100

    Add day 3

---
+++ day3/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func toPriority(r rune) int {
+	ri := int(r)
+
+	// Is uppercase?
+	if ri < 97 {
+		return ri - 38
+	}
+
+	return ri - 96
+}
+
+func part1(lines []string) (sum int) {
+	for _, line := range lines {
+		left := line[:len(line)/2]
+		right := line[len(line)/2:]
+
+		for _, r := range right {
+			if strings.Contains(left, string(r)) {
+				sum += toPriority(r)
+				break
+			}
+		}
+	}
+
+	return sum
+}
+
+func part2(lines []string) (sum int) {
+	for i := 0; i < len(lines); i += 3 {
+		for _, r := range lines[i+2] {
+			if strings.Contains(lines[i], string(r)) &&
+				strings.Contains(lines[i+1], string(r)) {
+				sum += toPriority(r)
+				break
+			}
+		}
+	}
+
+	return sum
+}
+
+func main() {
+	var (
+		lines = strings.Split(input, "\n")
+	)
+
+	fmt.Printf("Part one: %d\n", part1(lines))
+	fmt.Printf("Part two: %d\n", part2(lines))
+}
